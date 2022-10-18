63d8898cd18a4e5e2a5830ed0149e4af292d2848
EricaMarkwardt <110263998+EricaMarkwardt@users.noreply.github.com>
2022-10-18 18:38:44 -0500

    Update day trip generator.py
    "Added variables into user input feedback."

--- day trip generator.py
+++ day trip generator.py
@@ -18,36 +18,37 @@ create_my_dream_vacation = {
 print(create_my_dream_vacation)    
 print("")
 
-destination_approval = input("Do you like your destination option? \n")
+destination_approval = input(f"Do you like your destination option, {destination}? \n")
 while destination_approval != "yes":
     new_destination = (random.choice(destination_choice))
     print(new_destination)
-    destination_approval = input("Do you like your new destination option? \n")
+    destination_approval = input(f"Do you like your new destination option, {new_destination}? \n")
     if destination_approval == "yes":
         create_my_dream_vacation["destination"] = new_destination
 print("Great choice!\n")
-restaurant_approval = input("Do you like your restaurant option? \n")
+restaurant_approval = input(f"Do you like your restaurant option, {restaurant}? \n")
 while restaurant_approval != "yes":
     new_restaurant = (random.choice(restaurant_choice))
     print(new_restaurant)
-    restaurant_approval = input("Do you like your new retaurant option? \n")
+    restaurant_approval = input(f"Do you like your new retaurant option, {new_restaurant}? \n")
     if restaurant_approval == "yes":
         create_my_dream_vacation["restaurant"] = new_restaurant
 print("Great choice!\n")
-transportation_approval = input("Do you like your travel option? \n")
+transportation_approval = input(f"Do you like your travel option, {transportation}? \n")
 while transportation_approval != "yes":
     new_transportation = (random.choice(mode_of_transportation))
     print(new_transportation)
-    transportation_approval = input("Do you like your new travel option? \n")
+    transportation_approval = input(f"Do you like your new travel option, {new_transportation}? \n")
     if transportation_approval == "yes":
         create_my_dream_vacation["transportation"] = new_transportation
 print("Great choice!\n")
-fun_approval = input("Do you like your entertainment option? \n")
+fun_approval = input(f"Do you like your entertainment option, {fun}? \n")
 while fun_approval != "yes":
     new_entertainment = (random.choice(entertainment))
     print(new_entertainment)
-    fun_approval = input("Do you like your new entertainment option? \n")
+    fun_approval = input(f"Do you like your new entertainment option, {new_entertainment}? \n")
     if fun == "yes":
         create_my_dream_vacation["destination"] = entertainment
 print("Great choice!\n")
-print(f"Your dream vacation is {create_my_dream_vacation}. We'll see you soon!")
+print(f"Here is your dream vacation. {create_my_dream_vacation}. We'll see you soon!")
+
